c62071c45441b26685d8913a354a6f6d8136e765
mauerhof <valentin.mauerhofer@unige.ch>
2019-03-21 10:52:09 +0100

    deleted files

--- Krome_utils/conc.py
+++
@@ -1,6 +0,0 @@
-filenames = ['outstromgrenSi0.dat','outstromgrenSi1.dat','outstromgrenSi2.dat','outstromgrenSi3.dat']
-with open('outstromgrenSi.dat', 'w') as outfile:
-    for fname in filenames:
-        with open(fname) as infile:
-            for line in infile:
-                outfile.write(line)
